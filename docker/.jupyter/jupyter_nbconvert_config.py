378ff3323357440af98cb2812ba56aefb3a25474
sidazhou <sidazhou@gmail.com>
2018-07-25 14:18:08 +0800

    updated docker config

--- docker/.jupyter/jupyter_nbconvert_config.py
+++ docker/.jupyter/jupyter_nbconvert_config.py
@@ -5,5 +5,5 @@
 # c.Exporter.preprocessors.append('jupyter_contrib_nbextensions.nbconvert_support.pre_codefolding')
 c.Exporter.preprocessors.append('jupyter_contrib_nbextensions.nbconvert_support.CodeFoldingPreprocessor')
 
-# working for cmd line
+# working for cmd line, but this also removes folded code when running ipynb from cmd line
 c.CodeFoldingPreprocessor.remove_folded_code=True
--- docker/.jupyter/jupyter_notebook_config.py
+++ docker/.jupyter/jupyter_notebook_config.py
@@ -151,8 +151,8 @@
 #c.NotebookApp.ignore_minified_js = False
 
 ## (bytes/sec) Maximum rate at which stream output can be sent on iopub before
-#  they are limited.
-#c.NotebookApp.iopub_data_rate_limit = 1000000
+#  they are limited. Default 100000
+c.NotebookApp.iopub_data_rate_limit = 10000000000
 
 ## (msgs/sec) Maximum rate at which messages can be sent on iopub before they are
 #  limited.
