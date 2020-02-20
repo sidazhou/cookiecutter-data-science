d1912537fd9f65802dd07cdf5864a7f46540093f
sidazhou <sidazhou@gmail.com>
2020-02-20 17:10:44 +0800

    Update shell.py

--- src/sdutils/shell.py
+++ src/sdutils/shell.py
@@ -2,12 +2,15 @@ import os
 import subprocess
 
 # run in scripts path
-def shell_run(cmd):
+def shell_run(cmd, my_env_variables={}):
     cmd_dir = os.path.dirname(os.path.abspath(cmd))
+    my_env = {**os.environ, **my_env_variables}
+    print(my_env)
     
     wd = os.getcwd()
     os.chdir(cmd_dir)
     process = subprocess.Popen(cmd,
+                         env=my_env,
                          stdout=subprocess.PIPE, 
                          stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
